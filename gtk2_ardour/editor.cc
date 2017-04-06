c491191f4ed39d7ae187874f5c39d93bdaef44bf
nick_m <mainsbridge@gmail.com>
2017-04-07 03:22:05 +1000

    no-op : add braces.

--- gtk2_ardour/editor.cc
+++ gtk2_ardour/editor.cc
@@ -3690,8 +3690,9 @@ Editor::duplicate_range (bool with_dialog)
 	} else if (get_smart_mode()) {
 		if (selection->time.length()) {
 			duplicate_selection (times);
-		} else
+		} else {
 			duplicate_some_regions (rs, times);
+		}
 	} else {
 		duplicate_some_regions (rs, times);
 	}
